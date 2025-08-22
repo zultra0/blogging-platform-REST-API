48132b9b0f39898b9284a175e1488f00302ab52a
Mikhail <mikhail.manco.dev@gmail.com>
2025-08-22 00:10:12 -0500

    v0.0.1

---
+++ routes/posts.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getAllPosts,
+  getPostById,
+  createPost,
+  updatePost,
+  deleteAllPosts,
+  deletePost,
+} from "../controllers/posts.js";
+
+const router = express.Router();
+
+router.get("/", getAllPosts);
+router.get("/:id", getPostById);
+router.post("/", createPost);
+router.put("/:id", updatePost);
+router.delete("/", deleteAllPosts);
+router.delete("/:id", deletePost);
+
+export default router;
